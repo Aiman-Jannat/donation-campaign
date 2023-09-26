eb718f8404a3582761c9c04604b59dbcf055afeb
Aiman-Jannat <ummeaimanjannat@gmail.com>
2023-09-26 22:00:10 +0600

    pie chart added

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,12 +1,59 @@
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import { getStoredDonations } from "../../utility/localStorage";
+
+import { useLoaderData } from "react-router-dom";
+
 
 
 
 const Statistics = () => {
     
+    const have = useLoaderData();
+    const local = getStoredDonations();
+    let percen1 = (parseFloat(local.length)/parseFloat(have.length))*100;
+    let percen2 = 100-percen1;
+    // console.log(percen1, percen2);
+    const COLORS = ['#FF444A', '#00C49F'];
+    const data1 = [
+        { "percentage": percen1,
+        "Name": 'Donation by me'
+    },
+        {"percentage": percen2,
+        "Name": 'Total Donation'
+    }
+]
+        
+        
+       
+    
+    // const data2 = [
+    //     { "percentage": percen2 }
+
+        
+       
+    // ];
+    
+    
     
     return (
         <div>
-            
+            <PieChart width={700} height={700} className='mx-auto'>
+            <Pie
+          data={data1} 
+          dataKey="percentage"
+          
+          
+          outerRadius={250} 
+          fill="#FF444A"
+          
+        >
+        {
+        data1.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+        }
+        {percen1}
+        </Pie>
+        </PieChart>
+           
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,7 @@ const router = createBrowserRouter([
       {
         path: '/statistics',
         element: <Statistics></Statistics>,
+        loader: ()=>fetch('/donation.json')
         
       },
       {
